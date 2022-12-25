520ac7d9c347a4223a22ee0f3ac2da164dbc3027
ritik-prog <ritikmakhija2002@gmail.com>
2022-12-25 14:29:41 +0530

    STATUS PAGE DESIGN AND BACKEND COMPLETED

--- helphub/STATUS.Designer.cs
+++ helphub/STATUS.Designer.cs
@@ -27,12 +27,29 @@ namespace helphub
             this.Button1 = new System.Windows.Forms.Button();
             this.Label1 = new System.Windows.Forms.Label();
             this.GroupBox1 = new System.Windows.Forms.GroupBox();
-            this.statusdataview = new System.Windows.Forms.DataGridView();
+            this.requestdataview = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Request_dataview = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.complaindataview = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aadharno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typeofcomplain = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mobilenumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aboutcomplain = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status_table = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.GroupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.statusdataview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.requestdataview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.complaindataview)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -55,16 +72,19 @@ namespace helphub
             this.Label1.AutoSize = true;
             this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Label1.ForeColor = System.Drawing.Color.Black;
-            this.Label1.Location = new System.Drawing.Point(457, 174);
+            this.Label1.Location = new System.Drawing.Point(546, 217);
             this.Label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(974, 67);
+            this.Label1.Size = new System.Drawing.Size(640, 67);
             this.Label1.TabIndex = 12;
-            this.Label1.Text = "STATUS(COMPLAINS/REQUEST)";
+            this.Label1.Text = "COMPLAINS STATUS";
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // GroupBox1
             // 
-            this.GroupBox1.Controls.Add(this.statusdataview);
+            this.GroupBox1.Controls.Add(this.requestdataview);
+            this.GroupBox1.Controls.Add(this.label2);
+            this.GroupBox1.Controls.Add(this.complaindataview);
             this.GroupBox1.Controls.Add(this.pictureBox3);
             this.GroupBox1.Controls.Add(this.pictureBox2);
             this.GroupBox1.Controls.Add(this.Label1);
@@ -77,23 +97,195 @@ namespace helphub
             this.GroupBox1.TabIndex = 28;
             this.GroupBox1.TabStop = false;
             // 
-            // statusdataview
+            // requestdataview
             // 
-            this.statusdataview.AllowUserToAddRows = false;
-            this.statusdataview.AllowUserToDeleteRows = false;
-            this.statusdataview.AllowUserToResizeColumns = false;
-            this.statusdataview.AllowUserToResizeRows = false;
-            this.statusdataview.BackgroundColor = System.Drawing.Color.White;
-            this.statusdataview.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.statusdataview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.statusdataview.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.statusdataview.GridColor = System.Drawing.Color.Black;
-            this.statusdataview.Location = new System.Drawing.Point(68, 341);
-            this.statusdataview.Name = "statusdataview";
-            this.statusdataview.RowHeadersWidth = 82;
-            this.statusdataview.RowTemplate.Height = 41;
-            this.statusdataview.Size = new System.Drawing.Size(1531, 643);
-            this.statusdataview.TabIndex = 29;
+            this.requestdataview.AllowUserToAddRows = false;
+            this.requestdataview.AllowUserToDeleteRows = false;
+            this.requestdataview.AllowUserToResizeColumns = false;
+            this.requestdataview.AllowUserToResizeRows = false;
+            this.requestdataview.BackgroundColor = System.Drawing.Color.White;
+            this.requestdataview.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.requestdataview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.requestdataview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.Request_dataview,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7});
+            this.requestdataview.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.requestdataview.GridColor = System.Drawing.Color.Black;
+            this.requestdataview.Location = new System.Drawing.Point(86, 753);
+            this.requestdataview.Name = "requestdataview";
+            this.requestdataview.RowHeadersWidth = 82;
+            this.requestdataview.RowTemplate.Height = 41;
+            this.requestdataview.Size = new System.Drawing.Size(1485, 322);
+            this.requestdataview.TabIndex = 31;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ID";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "aadharno";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Aadhar Number";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 200;
+            // 
+            // Request_dataview
+            // 
+            this.Request_dataview.DataPropertyName = "typeofrequest";
+            this.Request_dataview.HeaderText = "Request";
+            this.Request_dataview.MinimumWidth = 10;
+            this.Request_dataview.Name = "Request_dataview";
+            this.Request_dataview.ReadOnly = true;
+            this.Request_dataview.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "mobilenumber";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Mobile Number";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "aboutrequest";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Request Details";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "address";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Address";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            this.dataGridViewTextBoxColumn6.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "status";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Status";
+            this.dataGridViewTextBoxColumn7.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            this.dataGridViewTextBoxColumn7.Width = 200;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(580, 656);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(576, 67);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "REQUEST STATUS";
+            // 
+            // complaindataview
+            // 
+            this.complaindataview.AllowUserToAddRows = false;
+            this.complaindataview.AllowUserToDeleteRows = false;
+            this.complaindataview.AllowUserToResizeColumns = false;
+            this.complaindataview.AllowUserToResizeRows = false;
+            this.complaindataview.BackgroundColor = System.Drawing.Color.White;
+            this.complaindataview.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.complaindataview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.complaindataview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.aadharno,
+            this.typeofcomplain,
+            this.mobilenumber,
+            this.aboutcomplain,
+            this.address,
+            this.status_table});
+            this.complaindataview.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.complaindataview.GridColor = System.Drawing.Color.Black;
+            this.complaindataview.Location = new System.Drawing.Point(86, 331);
+            this.complaindataview.Name = "complaindataview";
+            this.complaindataview.RowHeadersWidth = 82;
+            this.complaindataview.RowTemplate.Height = 41;
+            this.complaindataview.Size = new System.Drawing.Size(1485, 322);
+            this.complaindataview.TabIndex = 29;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 10;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 200;
+            // 
+            // aadharno
+            // 
+            this.aadharno.DataPropertyName = "aadharno";
+            this.aadharno.HeaderText = "Aadhar Number";
+            this.aadharno.MinimumWidth = 10;
+            this.aadharno.Name = "aadharno";
+            this.aadharno.ReadOnly = true;
+            this.aadharno.Width = 200;
+            // 
+            // typeofcomplain
+            // 
+            this.typeofcomplain.DataPropertyName = "typeofcomplain";
+            this.typeofcomplain.HeaderText = "Complain";
+            this.typeofcomplain.MinimumWidth = 10;
+            this.typeofcomplain.Name = "typeofcomplain";
+            this.typeofcomplain.ReadOnly = true;
+            this.typeofcomplain.Width = 200;
+            // 
+            // mobilenumber
+            // 
+            this.mobilenumber.DataPropertyName = "mobilenumber";
+            this.mobilenumber.HeaderText = "Mobile Number";
+            this.mobilenumber.MinimumWidth = 10;
+            this.mobilenumber.Name = "mobilenumber";
+            this.mobilenumber.ReadOnly = true;
+            this.mobilenumber.Width = 200;
+            // 
+            // aboutcomplain
+            // 
+            this.aboutcomplain.DataPropertyName = "aboutcomplain";
+            this.aboutcomplain.HeaderText = "Complain Details";
+            this.aboutcomplain.MinimumWidth = 10;
+            this.aboutcomplain.Name = "aboutcomplain";
+            this.aboutcomplain.ReadOnly = true;
+            this.aboutcomplain.Width = 200;
+            // 
+            // address
+            // 
+            this.address.DataPropertyName = "address";
+            this.address.HeaderText = "Address";
+            this.address.MinimumWidth = 10;
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            this.address.Width = 200;
+            // 
+            // status_table
+            // 
+            this.status_table.DataPropertyName = "status";
+            this.status_table.HeaderText = "Status";
+            this.status_table.MinimumWidth = 10;
+            this.status_table.Name = "status_table";
+            this.status_table.ReadOnly = true;
+            this.status_table.Width = 200;
             // 
             // pictureBox3
             // 
@@ -104,6 +296,7 @@ namespace helphub
             this.pictureBox3.Size = new System.Drawing.Size(200, 58);
             this.pictureBox3.TabIndex = 28;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox2
             // 
@@ -114,6 +307,7 @@ namespace helphub
             this.pictureBox2.Size = new System.Drawing.Size(160, 55);
             this.pictureBox2.TabIndex = 27;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
@@ -132,21 +326,22 @@ namespace helphub
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(1662, 989);
+            this.ClientSize = new System.Drawing.Size(1662, 1089);
             this.Controls.Add(this.Button1);
             this.Controls.Add(this.GroupBox1);
             this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1688, 1060);
+            this.MaximumSize = new System.Drawing.Size(1688, 1160);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(1688, 1060);
+            this.MinimumSize = new System.Drawing.Size(1688, 1160);
             this.Name = "STATUS";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "STATUS - HELPHUB";
             this.GroupBox1.ResumeLayout(false);
             this.GroupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.statusdataview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.requestdataview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.complaindataview)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -159,6 +354,23 @@ namespace helphub
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
-        private DataGridView statusdataview;
+        private DataGridView complaindataview;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn aadharno;
+        private DataGridViewTextBoxColumn typeofcomplain;
+        private DataGridViewTextBoxColumn REQUEST;
+        private DataGridViewTextBoxColumn mobilenumber;
+        private DataGridViewTextBoxColumn aboutcomplain;
+        private DataGridViewTextBoxColumn address;
+        private DataGridViewTextBoxColumn status_table;
+        public Label label2;
+        private DataGridView requestdataview;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn Request_dataview;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
     }
 }
--- helphub/STATUS.cs
+++ helphub/STATUS.cs
@@ -26,21 +26,38 @@ namespace helphub
                 string SQLitecnStr = @"Data Source=./helphub.db";
                 SQLiteConnection SQLiteConn = new SQLiteConnection();
                 SQLiteCommand SQLitecmd = new SQLiteCommand();
+                SQLiteCommand SQLitecmd1 = new SQLiteCommand();
                 SQLiteConn.ConnectionString = SQLitecnStr;
                 SQLiteConn.Open();
+                // connection + query
                 SQLitecmd.Connection = SQLiteConn;
                 SQLitecmd.CommandText = "SELECT * FROM complaint WHERE aadharno='56436546453654'";
+                // connection + query
+                SQLitecmd1.Connection = SQLiteConn;
+                SQLitecmd1.CommandText = "SELECT * FROM request WHERE aadharno='54352352345'";
+                // data adapter
                 SQLiteDataAdapter da = new SQLiteDataAdapter(SQLitecmd);
+                SQLiteDataAdapter da1 = new SQLiteDataAdapter(SQLitecmd1);
+                // data table
                 DataTable dt = new DataTable();
+                DataTable dt1 = new DataTable();
+                // filling data in datatable with adapter
                 da.Fill(dt);
+                da1.Fill(dt1);
 
                 if (dt.Rows.Count > 0)
                 {
-                    statusdataview.DataSource = dt;
+                    complaindataview.DataSource = dt;
+                    requestdataview.DataSource = dt1;
                 }
                 else
                 {
-                    MessageBox.Show("Wrong Credentials", "LOGIN", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("No records found", "STATUS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    DASHBOARD dashboard = new DASHBOARD();
+
+                    dashboard.Show();
+
+                    this.Hide(); //Close Form1,the current open form.
                 }
 
                 SQLiteConn.Close();
@@ -48,9 +65,32 @@ namespace helphub
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Login Failed", ex.Message);
+                MessageBox.Show("Unable to Load Data", ex.Message);
                 Console.WriteLine(ex);
             }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            DASHBOARD dashboard = new DASHBOARD();
+
+            dashboard.Show();
+
+            this.Hide(); //Close Form1,the current open form.
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            LOGIN login = new LOGIN();
+
+            login.Show();
+
+            this.Hide(); //Close Form1,the current open form.
         }
     }
 }
