fdc0ab11a48e1cfd51fbf9d4860c8d0b4ab7915b
ritik-prog <ritikmakhija2002@gmail.com>
2022-12-25 14:35:06 +0530

    UserData class Added

---
+++ helphub/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helphub
+{
+    public class UserData
+    {
+        public static string ID = "";
+        public static string username = "";
+        public static string aadharno = "";
+        public static string mobilenumber = "";
+        public static string password = "";
+        public static string email = "";
+    }
+}
