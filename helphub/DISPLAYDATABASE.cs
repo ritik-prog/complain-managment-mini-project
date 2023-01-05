a985e4a92f3bf8055d434e885115188febb6bae6
RITIK MAKHIJA <ritikmakhija2002@gmail.com>
2023-01-06 04:59:48 +0530

    fetch fix

--- helphub/DISPLAYDATABASE.cs
+++ helphub/DISPLAYDATABASE.cs
@@ -20,37 +20,42 @@ namespace helphub
         SuperAdminDatabaseOperation control = new SuperAdminDatabaseOperation();
         void fetchdata()
         {
-            if (title == "USER DATABASE")
-            {
-                control.fetchusers();
-                displaydatabasedata.DataSource = control.dt;
-            }
-            else if (title == "SUPERVISOR DATABASE")
-            {
-                control.fetchsupervisor();
-                displaydatabasedata.DataSource = control.dt;
-            }
-            else if (title == "ADMIN DATABASE")
-            {
-                control.fetchadmin();
-                displaydatabasedata.DataSource = control.dt;
-            }
-            else if (title == "SUPER ADMIN DATABASE")
-            {
-                control.fetchsuperadmin();
-                displaydatabasedata.DataSource = control.dt;
-                updatedatabase.Visible = false;
-                adduser.Visible = false;
-                if (UserData.role == "root")
+            try{
+                if (title == "USER DATABASE")
                 {
-                    updatedatabase.Visible = true;
-                    adduser.Visible = true;
+                    control.fetchusers();
+                    displaydatabasedata.DataSource = control.dt;
                 }
-            }
-            else if (title == "STATE ADMIN DATABASE")
-            {
-                control.fetchstateadmin();
-                displaydatabasedata.DataSource = control.dt;
+                else if (title == "SUPERVISOR DATABASE")
+                {
+                    control.fetchsupervisor();
+                    displaydatabasedata.DataSource = control.dt;
+                }
+                else if (title == "ADMIN DATABASE")
+                {
+                    control.fetchadmin();
+                    displaydatabasedata.DataSource = control.dt;
+                }
+                else if (title == "SUPER ADMIN DATABASE")
+                {
+                    control.fetchsuperadmin();
+                    displaydatabasedata.DataSource = control.dt;
+                    updatedatabase.Visible = false;
+                    adduser.Visible = false;
+                    if (UserData.role == "root")
+                    {
+                        updatedatabase.Visible = true;
+                        adduser.Visible = true;
+                    }
+                }
+                else if (title == "STATE ADMIN DATABASE")
+                {
+                    control.fetchstateadmin();
+                    displaydatabasedata.DataSource = control.dt;
+                }
+            }catch(Exception ex){
+                CreateLogs.createlogobj.superadminlog(UserData.username, "SuperAdmin not able to fetch database - error:- " + ex.Message + "", this.Name, UserData.role);
+                MessageBox.Show("Not able to fetch data....", ex.Message);
             }
         }
         public DISPLAYDATABASE(string title)
